ca59dff05b87f9cc2d74e8f526ed71e0a68e4d96
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-06-11 23:04:42 +0500

    1 commit

---
+++ api/app.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import postRoute from './routes/post.route.js'
+const app = express()
+app.use('/api/posts', postRoute)
+app.listen(8800, ()=>{
+    console.log('app is running on port')
+})
---
+++ api/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req, res) =>{
+    //db operation
+ 
+}
+export const login = (req, res) =>{
+    //db operation
+    
+}
+export const logout = (req, res) =>{
+    //db operation
+}
