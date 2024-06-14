a8612172b498d3700eb6426670b7dce048f9243d
abdulwasay <abdulwasayhashmi983@gmail.com>
2024-06-14 22:50:10 +0500

    more of cleint side working

--- cleint/src/routes/listpage/Listpage.jsx
+++ cleint/src/routes/listpage/Listpage.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { listData } from '../../lib/dummyData'
 import "../listpage/listpage.scss"
-const Listname = () => {
+import Map from '../../component/map/Map.jsx'
+import Filter from '../../component/filter/Filter'
+import Card from '../../component/card/Card'
+
+const Listpage = () => {
+  const data = listData;
   return (
-    <div>Listname</div>
+    <div className='listPage'>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter/>
+          {data.map(item =>(
+            <Card key={item.id} item={item}/>
+          ))}
+        </div>
+      </div>
+    <div className="mapContainer">
+   <Map items={data}/>
+    </div>
+    </div>
   )
 }
 
-export default Listname
+export default Listpage
--- cleint/src/routes/singlepage/Singlepage.jsx
+++ cleint/src/routes/singlepage/Singlepage.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
-import "../singlepage/singlepage.scss"
+import '../singlepage/singlepage.scss'
+import Map from '../../component/map/Map.jsx'
+import Slider from '../../component/silder/Slider'
+import {singlePostData, userData} from "../../lib/dummyData"
 const Singlepage = () => {
   return (
-    <div>Singlepage</div>
+    <div className='singlePage'>
+      <div className="details">
+        <div className="wrapper">
+<Slider images={singlePostData.images}/>
+<div className="info">
+  <div className="top">
+<div className="post">
+  <h1>{singlePostData.title}</h1>
+  <div className="address">
+    <img src="/pin.png" alt="" />
+    <span>{singlePostData.address}</span>
+  </div>
+  <div className="price">$ {singlePostData.price}</div>
+</div>
+<div className="user">
+<img src={userData.img} alt="" />
+<span>{userData.name}</span>
+</div>
+  </div>
+  <div className="bottom">
+     {singlePostData.description}
+  </div>
+</div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper">
+<p className="tiltle">General</p>
+<div className="listVertical"></div>
+<p className="tiltle">Sizes</p>
+<div className="sizes"></div>
+<p className="tiltle">Nearby Places</p>
+<div className="listHorizontal"></div>
+<p className="tiltle">Location</p>
+<div className="mapContainer">
+  <Map items={singlePostData}/>
+</div>
+<div className="buttons">
+  <button>
+    <img src='/chat.png'/>
+    Send a Message
+  </button>
+  <button>
+    <img src='/save.png'/>
+    Save a Place
+  </button>
+</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
